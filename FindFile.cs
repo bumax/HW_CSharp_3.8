64cc78a7ffbd3e9af8e794aa7734bff77905a01b
user <user@WIN-AKH758KNU81>
2024-02-10 21:37:52 +1000

    Добавьте файлы проекта.

---
+++ FindFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_CSharp_3._8
+{
+    internal class FindFile
+    {
+        public static List<string> find(string path, string extension, string text)
+        {
+            var res = new List<string>();
+            var res2 = new List<string>();
+            var dirs = Directory.GetDirectories(path: path);
+            foreach(var dir in dirs)
+            {
+                res = res.Concat(findInFile(dir, extension, text)).ToList();
+                res = res.Concat(find(dir, extension, text)).ToList();
+
+            }
+            return res;
+        }
+
+        private static List<string> findInFile(string path, string extension, string text) {
+            var res = new List<string>();
+            var files = Directory.GetFiles(path, "*." + extension);
+            foreach (var file in files)
+            {
+                using(var sr = new StreamReader(file))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        if (sr.ReadLine().Contains(text))
+                        {
+                            res.Add(file);
+                            break;
+                        }
+                    }
+
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace HW_CSharp_3._8
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                var res = FindFile.find(Directory.GetCurrentDirectory(), args[0], args[1]);
+                foreach (var file in res)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+        }
+    }
+}
